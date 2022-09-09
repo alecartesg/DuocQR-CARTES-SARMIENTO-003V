7b3a9dedf3e39d539cc3146aba92b0d32b262e37
alecartesg <moritasiempresincera@gmail.com>
2022-09-09 01:03:22 -0300

    Estudiante genera objeto

--- src/app/pages/registro-estudiante/registro-estudiante.page.ts
+++ src/app/pages/registro-estudiante/registro-estudiante.page.ts
@@ -8,6 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class RegistroEstudiantePage implements OnInit {
 
   constructor() { }
+  usuario = {
+    nombre: '',
+    email: '',
+    password: '',
+    lastname:'',
+    sede:'',
+    type: 'estudiante',
+    carrera:'',
+  }
+  onSubmit() {
+    console.log('Submit');
+    console.log(this.usuario);
+  }
 
   ngOnInit() {
   }
